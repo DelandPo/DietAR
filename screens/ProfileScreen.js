c979ae25597aaefc183f3811bd9eb5d974396747
Zhuoya Chen <zhuoyachen526@gmail.com>
2018-10-21 00:17:00 -0400

    colors

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -174,7 +174,7 @@ export default class ProfileScreen extends Component {
 
 const styles = StyleSheet.create({
   header: {
-    backgroundColor: "#00BFFF",
+    backgroundColor: "#BAF2BB",
     height: 200
   },
   avatar: {
@@ -229,6 +229,6 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     width: 250,
     borderRadius: 30,
-    backgroundColor: "#00BFFF"
+    backgroundColor: "#BAD7F2"
   }
 });
